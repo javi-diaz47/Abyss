23045fc31f52b0c11c0fc72889e67e452722c53c
javi-diaz47 <javiereduardo300@gmail.com>
2026-02-24 14:17:18 -0500

    ✨ feat(api-core): Add cache manager store

--- backend/src/app/main.py
+++ backend/src/app/main.py
@@ -1,6 +1,26 @@
+import logging
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
-app = FastAPI()
+from app.core.constants import STORAGE_DIR
+from app.core.store import Store
+
+logging.basicConfig(level=logging.INFO, format="%(levelname)s - %(name)s: %(message)s")
+logger = logging.getLogger(__name__)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    if not STORAGE_DIR.exists():
+        STORAGE_DIR.mkdir()
+        logger.info(f"Created Abyss-Store directory at {STORAGE_DIR}")
+    app.state.store = Store()
+    logger.info("Initialized Cache Manager")
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")
